f67f434e946c50c3f2f0179f87460f63a818a744
ljw0096 <ljw0096@naver.com>
2022-07-24 18:22:43 +0900

    feat(MainPage): PostCard 컴포넌트 마크업 구현

--- pages/main/index.tsx
+++ pages/main/index.tsx
@@ -1,5 +1,50 @@
+import styled from '@emotion/styled'
+import PostCard from '../../src/components/PostCard'
+
+const PostCardDummy = Array.from({ length: 5 }, (_, idx) => {
+  return {
+    id: idx.toString(),
+    title: '참치마요',
+    thumbnailImageUrl: 'https://via.placeholder.com/300x150',
+    avatarImageUrl: 'https://via.placeholder.com/50',
+    author: 'Lee',
+    likes: 10,
+    comments: 20
+  }
+})
+
 const Main = () => {
-  return <div>hi</div>
+  return (
+    <TempLayout>
+      {PostCardDummy.map((cardData) => {
+        return (
+          <PostCard
+            id={cardData.id}
+            key={cardData.id}
+            title={cardData.title}
+            thumbnailImageUrl={cardData.thumbnailImageUrl}
+            avatarImageUrl={cardData.avatarImageUrl}
+            author={cardData.author}
+            likes={cardData.likes}
+            comments={cardData.comments}
+          />
+        )
+      })}
+    </TempLayout>
+  )
 }
+
+const TempLayout = styled.div`
+  position: relative;
+  left: 20%;
+  width: 37.5rem;
+  height: 81.2rem;
+  border: 1px solid black;
+  overflow-y: scroll;
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`
 
 export default Main
---
+++ src/components/PostCard.tsx
@@ -0,0 +1,96 @@
+import styled from '@emotion/styled'
+import Avatar from './Avatar'
+import { IoMdHeartEmpty } from 'react-icons/io'
+import { BiComment } from 'react-icons/bi'
+import Link from 'next/link'
+
+interface Props {
+  id: string
+  title: string
+  thumbnailImageUrl: string
+  avatarImageUrl: string
+  author: string
+  likes: number
+  comments: number
+}
+
+const IMAGE_ALT = 'NO IMAGE'
+
+const PostCard = ({
+  id,
+  title,
+  thumbnailImageUrl,
+  avatarImageUrl,
+  author,
+  likes,
+  comments
+}: Props) => {
+  return (
+    <Link href={`/detail/${id}`}>
+      <CardContainer>
+        <Title>{title}</Title>
+        <ThumbnailImage src={thumbnailImageUrl} alt={IMAGE_ALT} />
+        <CardFooter>
+          <UserInfoWrapper>
+            <Avatar size={5} src={avatarImageUrl} isLoading={false} />
+            <Author>{author}</Author>
+          </UserInfoWrapper>
+          <PostInfoWrapper>
+            <IoMdHeartEmpty size={20} />
+            <Text>{likes}</Text>
+            <BiComment size={20} />
+            <Text>{comments}</Text>
+          </PostInfoWrapper>
+        </CardFooter>
+      </CardContainer>
+    </Link>
+  )
+}
+
+const CardContainer = styled.div`
+  width: 100%;
+  cursor: pointer;
+`
+
+const Title = styled.div`
+  padding: 2rem;
+  font-size: 2rem;
+  text-align: center;
+  box-sizing: border-box;
+`
+
+const ThumbnailImage = styled.img`
+  width: 100%;
+`
+
+const CardFooter = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+const UserInfoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  padding: 1rem;
+  box-sizing: border-box;
+`
+
+const Author = styled.div`
+  font-size: 1.6rem;
+  font-weight: bold;
+`
+
+const PostInfoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  padding: 1rem;
+  box-sizing: border-box;
+`
+
+const Text = styled.div`
+  font-size: 2rem;
+`
+
+export default PostCard
