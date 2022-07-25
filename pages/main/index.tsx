12129c5149bb61244320207d998c0a1411568b19
Jiwon Lee <ljw0096@naver.com>
2022-07-25 13:54:28 +0900

    fix(MainPage): PropsName 오타 수정 (#15)
    * fix(MainPage): PropsName 오타 수정
    
    * fix(MainPage): ThumbnailImage 컴포넌트 Image로 수정

--- pages/main/index.tsx
+++ pages/main/index.tsx
@@ -5,7 +5,7 @@ const PostCardDummy = Array.from({ length: 5 }, (_, idx) => {
   return {
     id: idx.toString(),
     title: '참치마요',
-    thumbnailImageUrl: 'https://via.placeholder.com/300x150',
+    imageUrl: 'https://via.placeholder.com/300x150',
     avatarImageUrl: 'https://via.placeholder.com/50',
     author: 'Lee',
     likes: 10,
@@ -22,7 +22,7 @@ const Main = () => {
             id={cardData.id}
             key={cardData.id}
             title={cardData.title}
-            thumbnailImageUrl={cardData.thumbnailImageUrl}
+            imageUrl={cardData.imageUrl}
             avatarImageUrl={cardData.avatarImageUrl}
             author={cardData.author}
             likes={cardData.likes}
