f9a271130a40d05f9cbed3fbdab67456d4a11e59
Shinyoung Hah <79133602+bellasimi@users.noreply.github.com>
2022-07-27 00:02:56 +0900

    feature: 기본 Input 컴포넌트 생성 (#25)
    * feature: Input 컴포넌트 생성
    
    * chore: prop에 width, height 추가
    
    * chore: 이벤트 타입 수정
    
    * refactor: interface 변경
    
    * chore: 오타 수정

---
+++ src/components/Input.tsx
@@ -0,0 +1,83 @@
+import { useCallback } from 'react'
+import styled from '@emotion/styled'
+
+interface Props {
+  width?: number
+  height?: number
+  type: string
+  name?: string
+  accept?: string
+  required?: boolean
+  isValid?: boolean
+  placeholder: string
+  onChange?: React.ChangeEventHandler<HTMLInputElement>
+  onBlur?: React.FocusEventHandler<HTMLInputElement>
+  className?: string
+}
+
+const Input = ({
+  width,
+  height,
+  type,
+  name,
+  accept,
+  required = false,
+  isValid = true,
+  placeholder,
+  onChange,
+  onBlur,
+  className
+}: Props) => {
+  const handleChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      onChange && onChange(e)
+    },
+    [onChange]
+  )
+
+  const handleBlur = useCallback(
+    (e: React.FocusEvent<HTMLInputElement, Element>) => {
+      onBlur && onBlur(e)
+    },
+    [onBlur]
+  )
+
+  return (
+    <StyledInput
+      width={width}
+      height={height}
+      type={type}
+      name={name}
+      accept={accept}
+      required={required}
+      isValid={isValid}
+      placeholder={placeholder}
+      onChange={handleChange}
+      onBlur={handleBlur}
+      className={className}
+    />
+  )
+}
+
+const StyledInput = styled.input<Props>`
+  width: ${({ width }) => `${width}rem`};
+  height: ${({ height }) => `${height}rem`};
+  border: 0.1rem solid
+    ${({ theme, isValid }) =>
+      isValid ? theme.color.borderNormal : theme.color.mainRed};
+  padding: 2.2rem 2.2rem;
+  border-radius: 1rem;
+  box-sizing: border-box;
+  ::placeholder {
+    color: #a3a3a3;
+  }
+  :-webkit-autofill {
+    -webkit-box-shadow: 0 0 0 100rem white inset;
+    box-shadow: 0 0 0 100rem white inset;
+  }
+  &:focus {
+    border: 0.1rem solid black;
+  }
+`
+
+export default Input
