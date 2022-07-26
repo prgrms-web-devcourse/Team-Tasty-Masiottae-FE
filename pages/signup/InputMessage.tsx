05a77563b0dac3866bddc3f9a17a3f684151657b
bellasimi <gksmf6611@gmail.com>
2022-07-27 01:41:37 +0900

    refactor: useValidate의 반환값을 직접 props로 받아 사용하도록 수정

--- pages/signup/InputMessage.tsx
+++ pages/signup/InputMessage.tsx
@@ -1,42 +1,22 @@
 import styled from '@emotion/styled'
 import theme from '@constants/theme'
-import {
-  INPUT_EMAIL,
-  INPUT_NICKNAME,
-  INPUT_PASSWORD,
-  INPUT_PASSWORD_CONFIRM,
-  INPUT_SNS,
-  MESSAGE_NICKNAME,
-  MESSAGE_PASSWORD,
-  ERROR_EXIST_NICKNAME,
-  ERROR_EXIST_EMAIL,
-  ERROR_EMAIL,
-  ERROR_PASSWORD_CONFIRM
-} from '@constants/inputConstant'
 
 interface Props {
-  name?: string
   isValid?: boolean
-  message: string
+  message?: string
 }
 
-const InputMessage = ({ name, isValid, message }: Props) => {
-  /*   const handleMessage = (name: string) => {
-    switch (name) {
-      case INPUT_EMAIL:
-        return !isValid && ERROR_EMAIL
-    }
-  } */
-
-  return <StyledMessage>{message}</StyledMessage>
+const InputMessage = ({ isValid = true, message }: Props) => {
+  return <StyledMessage isValid={isValid}>{message}</StyledMessage>
 }
 
-const StyledMessage = styled.span`
+const StyledMessage = styled.div<{ isValid?: boolean }>`
   text-align: left;
-  margin-top: 10px;
-  margin-left: 10px;
-  font-size: 14px;
-  color: ${theme.color.mainRed};
+  margin-top: 0.2rem;
+  margin-left: 1rem;
+  font-size: 1.4rem;
+  color: ${({ isValid }) =>
+    isValid ? theme.color.fontNormal : theme.color.mainRed};
 `
 
 export default InputMessage
