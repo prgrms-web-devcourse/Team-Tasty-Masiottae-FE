e9e31c7cc0ce6f486a56101ee9a4b8b7cc7e75f1
bellasimi <gksmf6611@gmail.com>
2022-07-27 04:47:51 +0900

    feature(Header): Link, Title 추가

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -3,23 +3,63 @@ import { useRouter } from 'next/router'
 import { VscChevronLeft } from 'react-icons/vsc'
 import { FiLogIn, FiUser } from 'react-icons/fi'
 import theme from '@constants/theme'
+import Link from 'next/link'
+import { useEffect, useState, useCallback } from 'react'
 
 const { mainPink, mainWhite, borderLight } = theme.color
 const { headerHeight, pagePadding } = theme.layout
+const MYINFO_URL = '/myInfo'
+const USER_URL = '/user'
+const HOME_URL = '/'
+const LOGIN_URL = '/login'
+const MYINFO = '내정보'
+const USER = '메뉴판'
 
 export const Header = () => {
   const router = useRouter()
+  const { pathname } = router
+  const [title, setTitle] = useState('')
   const onClickPrev = () => {
     router.back()
   }
 
+  const handlePathChange = useCallback((pathname: string) => {
+    if (pathname === MYINFO_URL) {
+      setTitle(MYINFO)
+    }
+    if (pathname.includes(USER_URL)) {
+      setTitle(USER)
+    }
+  }, [])
+
+  useEffect(() => {
+    setTitle('')
+    pathname && handlePathChange(pathname)
+  }, [pathname, handlePathChange])
+
   return (
     <HeaderContainer>
       <StyledBackIcon onClick={onClickPrev} />
-      <Logo>맛이 어때</Logo>
+      {title ? (
+        <Title>{title}</Title>
+      ) : (
+        <Link href={HOME_URL}>
+          <a>
+            <Logo>맛이 어때</Logo>
+          </a>
+        </Link>
+      )}
       <InnerRight>
-        <StyledUserIcon />
-        <StyledLoginIcon />
+        <Link href={MYINFO_URL}>
+          <a>
+            <StyledUserIcon />
+          </a>
+        </Link>
+        <Link href={LOGIN_URL}>
+          <a>
+            <StyledLoginIcon />
+          </a>
+        </Link>
       </InnerRight>
     </HeaderContainer>
   )
@@ -46,6 +86,16 @@ const StyledBackIcon = styled(VscChevronLeft)`
   width: 3rem;
   height: 3rem;
   cursor: pointer;
+`
+
+const Title = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 2rem;
+  font-weight: 700;
+  white-space: nowrap;
 `
 
 const Logo = styled.div`
