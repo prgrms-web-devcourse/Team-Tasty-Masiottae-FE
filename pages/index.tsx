ca29bc4c3421b34c39dad6ad5d9a1155d7c1df5d
ljw0096 <ljw0096@naver.com>
2022-07-26 17:08:57 +0900

    feature: useIntersectionObserver hook 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,18 @@
 import PostCard from '@components/PostCard'
+import { useState } from 'react'
+import useIntersectionObserver from '../src/hooks/useIntersectionObserver'
 
-const PostCardDummy = Array.from({ length: 5 }, (_, idx) => {
+const Card = {
+  id: '12341324',
+  title: '참치마요',
+  imageUrl: 'https://via.placeholder.com/300x150',
+  avatarImageUrl: 'https://via.placeholder.com/50',
+  author: 'Lee',
+  likes: 10,
+  comments: 20
+}
+
+const PostCardDummy = Array.from({ length: 4 }, (_, idx) => {
   return {
     id: idx.toString(),
     title: '참치마요',
@@ -13,20 +25,32 @@ const PostCardDummy = Array.from({ length: 5 }, (_, idx) => {
 })
 
 const Main = () => {
+  const [cards, setCards] = useState(PostCardDummy)
+  const ref = useIntersectionObserver(
+    async (entry, observer) => {
+      observer.unobserve(entry.target)
+      setCards([...cards, Card])
+    },
+    { threshold: 0.5 }
+  )
+
   return (
     <>
-      {PostCardDummy.map((cardData) => (
-        <PostCard
-          id={cardData.id}
-          key={cardData.id}
-          title={cardData.title}
-          imageUrl={cardData.imageUrl}
-          avatarImageUrl={cardData.avatarImageUrl}
-          author={cardData.author}
-          likes={cardData.likes}
-          comments={cardData.comments}
-        />
-      ))}
+      {cards.map((cardData, idx) => {
+        return (
+          <PostCard
+            id={cardData.id}
+            key={idx.toString()}
+            title={cardData.title}
+            imageUrl={cardData.imageUrl}
+            avatarImageUrl={cardData.avatarImageUrl}
+            author={cardData.author}
+            likes={cardData.likes}
+            comments={cardData.comments}
+            divRef={cards.length === idx + 1 ? ref : null}
+          />
+        )
+      })}
     </>
   )
 }
