8ec3620737329b144bd47f83e4c5878d6476ad39
nyoung <75849590+nyoung113@users.noreply.github.com>
2022-07-27 03:00:39 +0900

    style(EditPage) : 수정 페이지 마크업

--- pages/edit-menu/[id].tsx
+++ pages/edit-menu/[id].tsx
@@ -1,32 +1,124 @@
 import styled from '@emotion/styled'
+import Input from '@components/Input'
+import { useState } from 'react'
+import { TASTE_LIST } from '@constants/taste'
+import Tag from '@components/Tag'
+import { TasteType } from '@customTypes/index'
+
+const dummyBrands = [
+  '스타벅스',
+  '이디야',
+  '공차',
+  '아마스빈',
+  '피자헛',
+  '서브웨이'
+]
+
+interface Option {
+  name: string
+  description: string
+}
 
 const CreateMenu = () => {
-  let selectedTags: string[] = []
-  const handleClick = (clickedTag: string) => {
+  const [options, setOptions] = useState<Option[]>([])
+  let selectedTags: TasteType[] = []
+  const handleOptionBtnClick = () => {
+    console.log(options)
+    setOptions((options) => {
+      const newOptions = [...options, { name: '', description: '' }]
+      return newOptions
+    })
+  }
+  const handleClickTag = (clickedTag: TasteType) => {
     if (selectedTags.includes(clickedTag)) {
       selectedTags = selectedTags.filter((tag) => tag !== clickedTag)
     } else {
       selectedTags.push(clickedTag)
     }
   }
-
   return (
     <FlexContainer>
       <Title>메뉴 수정</Title>
       <ImageBox htmlFor="image-input"></ImageBox>
-      <Input placeholder="메뉴 명"></Input>
-      <Input placeholder="기본 메뉴 명"></Input>
       <FileInput id="image-input" type="file"></FileInput>
-      <TagContainer></TagContainer>
+      <InputWrapper>
+        <Select name="brand">
+          {dummyBrands.map((brand, idx) => (
+            <option key={idx} value={brand}>
+              {brand}
+            </option>
+          ))}
+        </Select>
+        <TitleInput
+          height={2.4}
+          type="text"
+          name="title"
+          required={true}
+          placeholder="커스텀 메뉴의 제목을 지어주세요"
+        />
+        <TitleInput
+          height={2.4}
+          type="text"
+          name="original-title"
+          required={true}
+          placeholder="기본이 되는 메뉴의 제목을 적어주세요"
+        />
+        <OptionButton onClick={handleOptionBtnClick}>+ 옵션</OptionButton>
+
+        {options.map((option, idx) => (
+          <Flex key={idx}>
+            <OptionName
+              height={2.4}
+              type="text"
+              name="option-name"
+              placeholder="옵션 명"
+            />
+            <OptionDescription
+              height={2.4}
+              type="text"
+              name="option-name"
+              placeholder="옵션 단위, 또는 설명"
+            />
+          </Flex>
+        ))}
+        <PriceInput
+          width={20}
+          height={2.4}
+          type="text"
+          name="price"
+          placeholder="최종 가격을 입력해주세요"
+        />
+      </InputWrapper>
+      <SubTitle>맛</SubTitle>
+      <TagContainer>
+        {TASTE_LIST.map((taste, idx) => (
+          <Tag
+            key={idx}
+            name={taste}
+            height={3.2}
+            onClick={handleClickTag}
+          ></Tag>
+        ))}
+      </TagContainer>
     </FlexContainer>
   )
 }
 export default CreateMenu
 
-const FlexContainer = styled.div`
+const Flex = styled.div`
   display: flex;
+`
+
+const FlexContainer = styled(Flex)`
   flex-direction: column;
   align-items: center;
+`
+
+const InputWrapper = styled.div`
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
 `
 
 const Title = styled.h1`
@@ -61,8 +153,33 @@ const TagContainer = styled.div`
   }
 `
 
-const Input = styled.input`
-  width: 80%;
+const Select = styled.select`
+  width: 100%;
   height: 3.2rem;
-  border: 2px solid black;
+`
+
+const TitleInput = styled(Input)`
+  width: 100%;
+`
+
+const OptionButton = styled.button`
+  width: 8rem;
+  height: 2.8rem;
+`
+
+const OptionName = styled(Input)`
+  width: 30%;
+`
+const OptionDescription = styled(Input)`
+  width: 70%;
+`
+
+const PriceInput = styled(Input)`
+  width: 70%;
+  align-self: end;
+`
+
+const SubTitle = styled.h3`
+  font-size: 2.4rem;
+  align-self: start;
 `
