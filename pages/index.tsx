5b760e4643ad4aabdf7a61a05f50a487710e0164
Jiwon Lee <ljw0096@naver.com>
2022-07-26 23:44:42 +0900

    feature: 무한스크롤 영역 구현 (#26)
    * refactor(MainPage): main 페이지 index 페이지로 수정
    
    * refactor(MainPage): 페이지 내부 Layout 컴포넌트 삭제
    
    * feature: useIntersectionObserver hook 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,58 @@
 import type { NextPage } from 'next'
+import PostCard from '@components/PostCard'
+import { useState } from 'react'
+import useIntersectionObserver from '../src/hooks/useIntersectionObserver'
+
+const Card = {
+  id: '12341324',
+  title: '참치마요',
+  imageUrl: 'https://via.placeholder.com/300x150',
+  avatarImageUrl: 'https://via.placeholder.com/50',
+  author: 'Lee',
+  likes: 10,
+  comments: 20
+}
+
+const PostCardDummy = Array.from({ length: 4 }, (_, idx) => {
+  return {
+    id: idx.toString(),
+    title: '참치마요',
+    imageUrl: 'https://via.placeholder.com/300x150',
+    avatarImageUrl: 'https://via.placeholder.com/50',
+    author: 'Lee',
+    likes: 10,
+    comments: 20
+  }
+})
 
 const Home: NextPage = () => {
-  return <div>Home</div>
-}
+  const [cards, setCards] = useState(PostCardDummy)
+  const ref = useIntersectionObserver(
+    async (entry, observer) => {
+      observer.unobserve(entry.target)
+      setCards([...cards, Card])
+    },
+    { threshold: 0.5 }
+  )
+
+  return (
+    <>
+      {cards.map((cardData, idx) => {
+        return (
+          <PostCard
+            id={cardData.id}
+            key={idx.toString()}
+            title={cardData.title}
+            imageUrl={cardData.imageUrl}
+            avatarImageUrl={cardData.avatarImageUrl}
+            author={cardData.author}
+            likes={cardData.likes}
+            comments={cardData.comments}
+            divRef={cards.length === idx + 1 ? ref : null}
+          />
+        )
+      })}
+    </>
+  )
 
 export default Home
