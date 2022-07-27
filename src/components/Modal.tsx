dc26d288961cc703c10694809a2f74091019012f
chogyejin <whrpwls96@naver.com>
2022-07-27 14:33:10 +0900

    feat: option에 따라 화면 하단에서 올라오는 drawer Modal 컴포넌트 구현

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -10,9 +10,17 @@ interface Props {
   height?: number
   visible: boolean
   onClose: () => void
+  option?: 'drawer'
 }
 
-const Modal = ({ children, width, height, visible = true, onClose }: Props) => {
+const Modal = ({
+  children,
+  width,
+  height,
+  visible,
+  onClose,
+  option
+}: Props) => {
   const ref = useClickAway(onClose)
   const [element, setElement] = useState<HTMLElement | null>(null)
 
@@ -24,10 +32,9 @@ const Modal = ({ children, width, height, visible = true, onClose }: Props) => {
     return <></>
   }
 
-  // Modal 컴포넌트를 root가 아닌 document.body에 붙임
   return ReactDOM.createPortal(
     <BackgroundDim visible={visible}>
-      <ModalContainer ref={ref} width={width} height={height}>
+      <ModalContainer ref={ref} width={width} height={height} option={option}>
         {children}
         <div></div>
       </ModalContainer>
@@ -36,7 +43,6 @@ const Modal = ({ children, width, height, visible = true, onClose }: Props) => {
   )
 }
 
-// Dim은 모달 외 부분
 const BackgroundDim = styled.div<{ visible: boolean }>`
   display: ${({ visible }) => (visible ? 'block' : 'none')};
   position: fixed;
@@ -45,15 +51,17 @@ const BackgroundDim = styled.div<{ visible: boolean }>`
   width: 100vw;
   height: 100vh;
   background-color: rgba(0, 0, 0, 0.5);
-  z-index: 1000;
+  z-index: 999;
 `
 
 const ModalContainer = styled.div<{
   width: number | undefined
   height: number | undefined
+  option: 'drawer' | undefined
 }>`
   position: fixed;
-  top: 50%;
+  top: ${({ option }) => (option !== 'drawer' ? '50%' : '')};
+  bottom: ${({ option }) => (option === 'drawer' ? 0 : '')};
   left: 50%;
   transform: translate(-50%, -50%);
   width: ${({ width }) => `${width}rem`};
@@ -61,6 +69,18 @@ const ModalContainer = styled.div<{
   padding: 0.8rem;
   background-color: white;
   box-shadow: 0 0.3rem 0.6rem rgba(0, 0, 0, 0.2);
+  animation: ${({ option }) => (option === 'drawer' ? 'drawer-show 0.3s' : '')};
+
+  @keyframes drawer-show {
+    from {
+      opacity: 0;
+      margin-bottom: -5rem;
+    }
+    to {
+      opacity: 1;
+      margin-bottom: 0;
+    }
+  }
 `
 
 export default Modal
