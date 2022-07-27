e637cb91fa6c3b560ffd44d1dc55ff300fa3ae39
nyoung <75849590+nyoung113@users.noreply.github.com>
2022-07-27 18:21:01 +0900

    rename(EditPage): 더미데이터 파일 분리

--- pages/edit-menu/[id].tsx
+++ pages/edit-menu/[id].tsx
@@ -2,48 +2,11 @@ import styled from '@emotion/styled'
 import Input from '@components/Input'
 import { useState } from 'react'
 import Tag from '@components/Tag'
-import { Taste } from '@customTypes/index'
-
-const dummyFranchiseList = [
-  { id: 0, name: '스타벅스' },
-  { id: 1, name: '이디야' },
-  { id: 2, name: '공차' },
-  { id: 3, name: '아마스빈' },
-  { id: 4, name: '서브웨이' }
-]
-
-const dummyTasteList: Taste[] = [
-  { id: 0, name: '차가운', color: '#00B5E3' },
-  { id: 1, name: '뜨거운', color: '#FF3333' },
-  { id: 2, name: '달콤한', color: '#CC0099' },
-  { id: 3, name: '매콤한', color: '#df2020' },
-  { id: 4, name: '새콤한', color: '#FFDD33' },
-  { id: 5, name: '쌉쌀한', color: '#339966' },
-  { id: 6, name: '짭짤한', color: '#FF5533' }
-]
-
-const dummyMenu = {
-  id: '1',
-  franchise: { id: 'franchise1', name: '스타벅스' },
-  image: '',
-  title: '슈렉 프라푸치노',
-  originalTitle: '제주 유기농 말차로 만든 크림 프라푸치노',
-  author: { id: 'user1' },
-  content: '설명' || null,
-  options: [
-    { name: '에스프레소샷', description: '1' },
-    { name: '자바칩', description: '약간' },
-    { name: '아이스크림', description: '한 스쿱' }
-  ],
-  expectedPrice: 6700,
-  tastes: [
-    { id: 0, name: '차가운', color: '#00B5E3' },
-    { id: 2, name: '달콤한', color: '#CC0099' }
-  ],
-  likes: 100,
-  createdAt: String,
-  updatedAt: String
-}
+import {
+  dummyFranchiseList,
+  dummyTasteList,
+  dummyMenu
+} from '@constants/dummyMenu'
 
 interface Option {
   name: string
@@ -66,7 +29,7 @@ const CreateMenu = () => {
     })
   }
   const handleClickTag = (clickedTagId: number) => {
-    selectedTags.map(({ id, name }) => id === clickedTagId)
+    selectedTags.map(({ id }) => id === clickedTagId)
   }
   return (
     <FlexContainer>
