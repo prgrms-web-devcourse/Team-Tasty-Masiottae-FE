4397c6a771085755f394b26acf5ad42b0c9df149
ljw0096 <ljw0096@naver.com>
2022-07-27 17:19:14 +0900

    feat(CategoryPage): CategoryBox 구현

---
+++ pages/search/index.tsx
@@ -0,0 +1,11 @@
+import CategoryBox from '@components/CategoryBox'
+
+const Category = () => {
+  return (
+    <CategoryBox imageUrl="https://picsum.photos/seed/picsum/100/100">
+      스타벅스
+    </CategoryBox>
+  )
+}
+
+export default Category
---
+++ src/components/CategoryBox.tsx
@@ -0,0 +1,55 @@
+import styled from '@emotion/styled'
+import { ReactNode } from 'react'
+
+interface Props {
+  imageUrl: string
+  children: ReactNode
+}
+
+const CategoryBox = ({ children, imageUrl }: Props) => {
+  return (
+    <BoxContainer imageUrl={imageUrl}>
+      <Text>{children}</Text>
+    </BoxContainer>
+  )
+}
+
+const BoxContainer = styled.div<{ imageUrl: string }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 10rem;
+  height: 10rem;
+
+  position: relative;
+  z-index: 1;
+  background-size: cover;
+  cursor: pointer;
+
+  ::after {
+    width: 100%;
+    height: 100%;
+    content: '';
+    border-radius: 1rem;
+    background: ${({ imageUrl }) => `no-repeat top center url(${imageUrl})`};
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    opacity: 0.7;
+  }
+
+  :hover {
+    ::after {
+      opacity: 0.99;
+    }
+  }
+`
+
+const Text = styled.div`
+  background-color: rgba(255, 255, 255, 0.1);
+  font-size: 2.2rem;
+  font-weight: bold;
+`
+
+export default CategoryBox
