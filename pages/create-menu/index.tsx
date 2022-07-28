fc0dfde559abe60f9f04b0476c24978c21b78f06
nyoung <75849590+nyoung113@users.noreply.github.com>
2022-07-28 17:35:08 +0900

    feat(IamgeUploader): 이미지 업로더 구현

--- pages/create-menu/index.tsx
+++ pages/create-menu/index.tsx
@@ -1,28 +1,14 @@
-import Tag from '@components/Tag'
 import styled from '@emotion/styled'
 import ImageUploader from '@components/ImageUploader'
-import { TasteType } from '@customTypes/index'
-import { TASTE_LIST } from '@constants/taste'
 
 const CreateMenu = () => {
-  let selectedTags: TasteType[] = []
-  const handleClick = (clickedTag: TasteType) => {
-    if (selectedTags.includes(clickedTag)) {
-      selectedTags = selectedTags.filter((tag) => tag !== clickedTag)
-    } else {
-      selectedTags.push(clickedTag)
-    }
+  const handleImageChange = (newImage: string | ArrayBuffer | null) => {
+    console.log(newImage) // image 받기 성공
   }
 
   return (
     <FlexContainer>
-      <Title>메뉴 등록</Title>
-      <ImageUploader />
-      <TagContainer>
-        {TASTE_LIST.map((taste, idx) => (
-          <Tag key={idx} name={taste} height={3.2} onClick={handleClick}></Tag>
-        ))}
-      </TagContainer>
+      <ImageUploader onChange={handleImageChange} />
     </FlexContainer>
   )
 }
@@ -32,36 +18,4 @@ const FlexContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-`
-
-const Title = styled.h1`
-  font-size: 4rem;
-  align-self: start;
-`
-
-const ImageBox = styled.div`
-  width: 30rem;
-  height: 30rem;
-  background-color: #d9d9d9;
-  &:hover {
-    cursor: pointer;
-  }
-`
-
-const FileInput = styled.input`
-  visibility: hidden;
-`
-
-const TagContainer = styled.div`
-  width: 80%;
-  height: 10rem;
-  display: flex;
-  flex-wrap: wrap;
-  padding: 0 0.5rem;
-  background-color: #d9d9d9;
-  overflow-y: scroll;
-  gap: 0.5rem;
-  ::-webkit-scrollbar {
-    display: none;
-  }
 `
--- src/components/ImageUploader.tsx
+++ src/components/ImageUploader.tsx
@@ -1,36 +1,81 @@
 import styled from '@emotion/styled'
+import React, { useEffect, useState } from 'react'
 import { BsPlusLg } from 'react-icons/bs'
 
-const ImageUploader = () => {
+const FILE_TYPE = 'image/gif, image/jpeg, image/png'
+type ImageType = string | ArrayBuffer | null
+interface Props {
+  size?: number
+  shape?: 'square' | 'circle'
+  value?: string | null
+  onChange: (image: ImageType) => void
+}
+
+const ImageUploader = ({
+  size,
+  shape = 'square',
+  value = null,
+  onChange
+}: Props) => {
+  const [image, setImage] = useState<ImageType>(value)
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const reader = new FileReader()
+    const file = e.target.files ? e.target.files[0] : null
+    if (file) {
+      reader.readAsDataURL(file)
+      reader.onload = () => {
+        setImage(reader.result)
+        onChange(reader.result)
+      }
+    }
+  }
+
   return (
     <>
-      <ImageBox htmlFor="image-loader"></ImageBox>
-      <StyledPlus />
-      <FileInput type="file" name="image-loader"></FileInput>
+      <ImageBox htmlFor="image-input" size={size} shape={shape} image={image}>
+        <StyledPlus loaded={image ? +true : +false} />
+      </ImageBox>
+      <FileInput
+        id="image-input"
+        type="file"
+        onChange={handleChange}
+        accept={FILE_TYPE}
+      />
     </>
   )
 }
 
-const ImageBox = styled.label`
-  width: 30rem;
-  height: 30rem;
+const ImageBox = styled.label<{
+  size: number | undefined
+  shape: 'square' | 'circle'
+  image: ImageType
+}>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: ${({ size }) => (size ? `${size}rem` : '100%')};
+  height: ${({ size }) => `${size}rem`};
+  padding-top: ${({ size }) => (size ? `0` : 'calc(50% - 5rem)')};
+  padding-bottom: ${({ size }) => (size ? `0` : 'calc(50% - 5rem)')};
   background-color: #d9d9d9;
+  background-image: ${({ image }) => (image ? `url(${image})` : null)};
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  border-radius: ${({ shape }) => (shape === 'circle' ? '50%' : '0')};
   &:hover {
     cursor: pointer;
   }
 `
 
+const StyledPlus = styled(BsPlusLg)<{ loaded: number }>`
+  width: 10rem;
+  height: 10rem;
+  visibility: ${({ loaded }) => (loaded ? 'hidden' : 'visible')};
+`
+
 const FileInput = styled.input`
   visibility: hidden;
-`
-const StyledPlus = styled(BsPlusLg)`
-  width: 5rem;
-  height: 5rem;
-  position: absolute;
-  top: 9rem;
-  left: 50%;
-  transform: translateX(-50%);
-  cursor: pointer;
 `
 
 export default ImageUploader
