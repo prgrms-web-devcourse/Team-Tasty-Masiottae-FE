e1b33456b257eac5142bfb85a712ebb841d73488
bellasimi <gksmf6611@gmail.com>
2022-07-28 16:16:19 +0900

    style: 내정보 마크업 완성

--- pages/myInfo/index.tsx
+++ pages/myInfo/index.tsx
@@ -1,5 +1,64 @@
-const MyInfo = () => {
-  return <>내정보 페이지</>
+import styled from '@emotion/styled'
+import UserData from './UserData'
+import UserProfile from './UserProfile'
+import Button from '@components/Button'
+import { LOGIN_URL, PASSWORD_CHANGE_URL } from '@constants/pageUrl'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+import useClickAway from '@hooks/useClickAway'
+
+const MyInfoPage = () => {
+  const router = useRouter()
+  const [isLogoutModal, setIsLogoutModal] = useState(false)
+
+  const onCloseLogout = () => {
+    setIsLogoutModal(false)
+  }
+
+  const logoutModalRef = useClickAway(onCloseLogout)
+
+  return (
+    <UserContainer>
+      <UserProfile />
+      <UserData />
+      <ChangePasswordButton onClick={() => router.push(PASSWORD_CHANGE_URL)}>
+        비밀번호 변경하기
+      </ChangePasswordButton>{' '}
+      <LogoutButton onClick={() => router.push(LOGIN_URL)}>
+        로그아웃
+      </LogoutButton>
+    </UserContainer>
+  )
 }
 
-export default MyInfo
+export default MyInfoPage
+
+const UserContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+  background-color: white;
+`
+
+const ChangePasswordButton = styled(Button)`
+  width: 100%;
+  height: 7rem;
+  margin-top: 4rem;
+  background-color: ${(props) => props.theme.color.mainBlack};
+  border-radius: 1rem;
+  color: white;
+  font-size: 1.7rem;
+  font-weight: 500;
+  cursor: pointer;
+`
+
+const LogoutButton = styled(Button)`
+  height: 7rem;
+  margin-top: 1rem;
+  background-color: ${(props) => props.theme.color.mainPink};
+  border-radius: 1rem;
+  color: white;
+  font-size: 1.7rem;
+  font-weight: 500;
+  cursor: pointer;
+`
