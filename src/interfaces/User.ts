2d20168070fe60c2ba5a62c63a809b3823b681ba
bellasimi <gksmf6611@gmail.com>
2022-07-30 23:49:29 +0900

    chore: UserType 인터페이스 추가

---
+++ src/interfaces/User.ts
@@ -0,0 +1,11 @@
+export interface UserType {
+  id: number
+  image: string
+  role: string
+  email: string
+  nickname: string
+  snsAccount: string
+  createdAt: string
+  updatedAt: string
+  menuCount: number
+}
