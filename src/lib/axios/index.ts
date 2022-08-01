32886a189dfbcb609982f3f326b0f8405f992545
Shinyoung Hah <79133602+bellasimi@users.noreply.github.com>
2022-08-01 15:01:37 +0900

    feat: axios create, interceptor 구현 (#65)
    * feat: axios create 생성
    
    * feat: axios interceptor에 사용할 함수들 추가
    
    * feat: localStorage에 token을 가져오기,저장, 삭제하는 함수 추가
    
    * chore: 함수명 변경
    
    * chore: 환경변수명 변경 및 불필요한 코드 삭제

---
+++ src/lib/axios/index.ts
@@ -0,0 +1,17 @@
+import axios from 'axios'
+import {
+  handleRequest,
+  handleRequestError,
+  handleResponse,
+  handleResponseError
+} from './interceptors'
+
+const axiosInstance = axios.create({
+  timeout: 5000,
+  baseURL: process.env.NEXT_PUBLIC_API_URL
+})
+
+axiosInstance.interceptors.request.use(handleRequest, handleRequestError)
+axiosInstance.interceptors.response.use(handleResponse, handleResponseError)
+
+export default axiosInstance
