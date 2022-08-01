3c4463bd732aad5d8c1327bab310106af27732a1
bellasimi <gksmf6611@gmail.com>
2022-08-02 01:33:40 +0900

    fix: interceptor localToken 에러, window is not defined 해결

--- src/lib/axios/localToken.ts
+++ src/lib/axios/localToken.ts
@@ -1,15 +1,18 @@
 const LOCAL_TOKEN_KEY = 'tastyToken'
-const storage = window.localStorage
 
 export const getLocalToken = () => {
-  const token = storage.getItem(LOCAL_TOKEN_KEY)
+  const token =
+    typeof window !== undefined && window.localStorage.getItem(LOCAL_TOKEN_KEY)
   return token
 }
 
 export const setLocalToken = (token: string) => {
-  return storage.setItem(LOCAL_TOKEN_KEY, token)
+  return (
+    typeof window !== undefined &&
+    window.localStorage.setItem(LOCAL_TOKEN_KEY, token)
+  )
 }
 
 export const removeLocalToken = () => {
-  storage.removeItem(LOCAL_TOKEN_KEY)
+  typeof window !== undefined && window.localStorage.removeItem(LOCAL_TOKEN_KEY)
 }
