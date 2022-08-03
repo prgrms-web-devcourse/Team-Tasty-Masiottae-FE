dd07d2c368998f477b91c17b7627d717456d3541
bellasimi <gksmf6611@gmail.com>
2022-08-04 03:25:20 +0900

    chore: localToken 위치 utils로 변경

---
+++ src/utils/localToken.ts
@@ -0,0 +1,25 @@
+const LOCAL_TOKEN_KEY = 'tastyToken'
+const LOCAL_TOKEN_EXPIRE_DATE = 'tastyToken_expire'
+
+interface Data {
+  accessToken: string
+  expirationTime: string
+}
+
+export const getLocalToken = () => {
+  const token =
+    typeof window !== undefined && window.localStorage.getItem(LOCAL_TOKEN_KEY)
+  return token
+}
+
+export const setLocalToken = ({ accessToken, expirationTime }: Data) => {
+  console.log(accessToken, expirationTime)
+  if (typeof window !== undefined) {
+    window.localStorage.setItem(LOCAL_TOKEN_KEY, accessToken)
+    window.localStorage.setItem(LOCAL_TOKEN_EXPIRE_DATE, expirationTime)
+  }
+}
+
+export const removeLocalToken = () => {
+  typeof window !== undefined && window.localStorage.removeItem(LOCAL_TOKEN_KEY)
+}
