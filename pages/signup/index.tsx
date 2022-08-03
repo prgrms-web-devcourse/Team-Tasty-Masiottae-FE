854bffb21111eb0b9688e8576d5d14ddfc45a676
bellasimi <gksmf6611@gmail.com>
2022-08-04 03:21:56 +0900

    feat: 회원가입 유효성 검사 구현

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -3,7 +3,8 @@ import { useCallback, useState } from 'react'
 import Input from '@components/Input'
 import Button from '@components/Button'
 import { BsEye } from 'react-icons/bs'
-import InputMessage from './InputMessage'
+import { useSignupMutation } from '@hooks/mutations/useSignupMutation'
+import axios from '@lib/axios'
 import {
   TEXT,
   PASSWORD,
@@ -19,16 +20,50 @@ import {
   PLACEHOLDER_SNS,
   MESSAGE_NICKNAME,
   MESSAGE_PASSWORD,
-  ERROR_EMAIL
+  ERROR_EMAIL,
+  ERROR_PASSWORD_CONFIRM,
+  REGEX_EMAIL,
+  REGEX_NICKNAME,
+  REGEX_PASSWORD,
+  MAX_PASSWORD,
+  MAX_NICKNAME
 } from '@constants/inputConstant'
+import ImageUploader from '@components/ImageUploader'
 
-const handleSignupSubmit: React.MouseEventHandler<HTMLButtonElement> = (e) => {
-  e.preventDefault()
+interface SignUpValues {
+  image?: File
+  email: string
+  nickName: string
+  password: string
+  passwordConfirm?: string
+  snsAccount?: string
+}
+
+const initialValues = {
+  email: '',
+  nickName: '',
+  password: '',
+  passwordConfirm: '',
+  snsAccount: ''
 }
 
 const Signup = () => {
   const [isTypePassword, setIsTypePassword] = useState(false)
   const [isTypeConfirmPassword, setIsTypeConfirmPassword] = useState(false)
+  const [values, setValues] = useState<SignUpValues>(initialValues)
+  const [errors, setErrors] = useState<SignUpValues>(initialValues)
+  const { mutate: postSignup } = useSignupMutation()
+
+  const handleSignUpSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    const isError = Object.keys(errors).filter(
+      (key) => errors[key as keyof SignUpValues] !== ''
+    ).length
+    const { email, nickName, password } = values
+    const isValues = email !== '' && nickName !== '' && password !== ''
+
+    !isError && isValues && postSignup(values)
+  }
 
   const handleEyeClick = useCallback((name: string) => {
     name === INPUT_PASSWORD
@@ -38,52 +73,135 @@ const Signup = () => {
         )
   }, [])
 
+  const handleImageChange = (file: File) => {
+    setValues({ ...values, image: file })
+  }
+
+  const handleCheckEmailClick = async (
+    e: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    e.preventDefault()
+    const property = 'email'
+    const value = values.email
+    const { data } = await axios.get(
+      `/accounts/check?property=${property}&value=${value}`
+    )
+    const { errorMessage } = data
+    if (errorMessage) {
+      setErrors({ ...errors, email: errorMessage })
+    }
+  }
+
+  const handleCheckNickNameClick = async (
+    e: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    e.preventDefault()
+    const property = 'nickName'
+    const value = values.nickName
+    const { data } = await axios.get(
+      `/accounts/check?property=${property}&value=${value}`
+    )
+    const { errorMessage } = data
+    if (errorMessage) {
+      setErrors({ ...errors, nickName: errorMessage })
+    }
+  }
+
+  const handleSignUpChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = e.target
+    setErrors(initialValues)
+
+    if (name === INPUT_EMAIL) {
+      if (!REGEX_EMAIL.test(value)) {
+        setErrors({ ...errors, [name]: ERROR_EMAIL })
+      }
+    }
+
+    if (name === INPUT_NICKNAME) {
+      e.target.value = value.replace(/\s/, '').slice(0, MAX_NICKNAME)
+
+      if (!REGEX_NICKNAME.test(value)) {
+        setErrors({ ...errors, [name]: MESSAGE_NICKNAME })
+      }
+    }
+
+    if (name === INPUT_PASSWORD) {
+      e.target.value = value.replace(/\s/, '').slice(0, MAX_PASSWORD)
+
+      if (!REGEX_PASSWORD.test(value)) {
+        setErrors({ ...errors, [name]: MESSAGE_PASSWORD })
+      }
+    }
+
+    if (name === INPUT_PASSWORD_CONFIRM) {
+      e.target.value = value.replace(/\s/, '').slice(0, MAX_PASSWORD)
+
+      if (!REGEX_PASSWORD.test(value)) {
+        setErrors({ ...errors, [name]: MESSAGE_PASSWORD })
+      }
+      if (values.password !== e.target.value) {
+        setErrors({ ...errors, [name]: ERROR_PASSWORD_CONFIRM })
+      }
+    }
+    setValues({ ...values, [name]: e.target.value })
+  }
+
   return (
-    <SignupForm>
+    <SignUpForm>
       <Title>회원 가입</Title>
-      {/* //TODO신영: 내영님의 이미지 업로드 컴포넌트로 교체 */}
+      <ImageUploader onChange={handleImageChange} />
       <InputContainer>
         <InputWrapper>
           <TextInput
             type={TEXT}
             name={INPUT_EMAIL}
             placeholder={PLACEHOLDER_EMAIL}
-          />
-          <InputMessage message={ERROR_EMAIL} isValid={false} />
+            onChange={handleSignUpChange}
+          />{' '}
+          <ExistCheckButton height={7} onClick={handleCheckEmailClick}>
+            중복 확인
+          </ExistCheckButton>
+          <ErrorText>{errors[INPUT_EMAIL]}</ErrorText>
         </InputWrapper>
         <InputWrapper>
           <TextInput
             type={TEXT}
             name={INPUT_NICKNAME}
             placeholder={PLACEHOLDER_NICKNAME}
-          />
-          <InputMessage message={MESSAGE_NICKNAME} />
+            onChange={handleSignUpChange}
+          />{' '}
+          <ExistCheckButton height={7} onClick={handleCheckNickNameClick}>
+            중복 확인
+          </ExistCheckButton>
+          <ErrorText>{errors[INPUT_NICKNAME]}</ErrorText>
         </InputWrapper>
         <InputWrapper>
           <TextInput
             type={isTypePassword ? TEXT : PASSWORD}
             name={INPUT_PASSWORD}
             placeholder={PLACEHOLDER_PASSWORD}
+            onChange={handleSignUpChange}
           />
           <ShowPasswordIcon
             onClick={() => {
               handleEyeClick(INPUT_PASSWORD)
             }}
           />
-          <InputMessage message={MESSAGE_PASSWORD} />
+          <ErrorText>{errors[INPUT_PASSWORD]}</ErrorText>
         </InputWrapper>
         <InputWrapper>
           <TextInput
             type={isTypeConfirmPassword ? TEXT : PASSWORD}
             name={INPUT_PASSWORD_CONFIRM}
             placeholder={PLACEHOLDER_PASSWORD_CONFIRM}
+            onChange={handleSignUpChange}
           />
           <ShowPasswordIcon
             onClick={() => {
               handleEyeClick(INPUT_PASSWORD_CONFIRM)
             }}
           />
-          <InputMessage />
+          <ErrorText>{errors[INPUT_PASSWORD_CONFIRM]}</ErrorText>
         </InputWrapper>
         <InputWrapper>
           <TextInput
@@ -91,17 +209,31 @@ const Signup = () => {
             name={INPUT_SNS}
             placeholder={PLACEHOLDER_SNS}
           />
-          <InputMessage message={MESSAGE_NICKNAME} />
         </InputWrapper>
       </InputContainer>
-      <SignupButton height={7} onClick={handleSignupSubmit}>
+      <SignUpButton height={7} onClick={handleSignUpSubmit}>
         회원 가입
-      </SignupButton>{' '}
-    </SignupForm>
+      </SignUpButton>
+    </SignUpForm>
   )
 }
 
-const SignupButton = styled(Button)`
+const ExistCheckButton = styled(Button)`
+  width: 8rem;
+  height: 5rem;
+  border-radius: 1rem;
+  background-color: black;
+  padding: 0 0.1rem;
+  color: white;
+  font-size: 1.5rem;
+  font-weight: 500;
+  position: absolute;
+  right: 1.2rem;
+  top: 1rem;
+  cursor: pointer;
+`
+
+const SignUpButton = styled(Button)`
   width: 100%;
   height: 7rem;
   margin-top: 1rem;
@@ -113,7 +245,7 @@ const SignupButton = styled(Button)`
   cursor: pointer;
 `
 
-const SignupForm = styled.form`
+const SignUpForm = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -128,7 +260,6 @@ const Title = styled.div`
 
 const InputContainer = styled.div`
   width: 100%;
-  margin-top: 3rem;
   display: flex;
   flex-direction: column;
   gap: 1rem;
@@ -144,6 +275,14 @@ const TextInput = styled(Input)`
   border-radius: 1rem;
   padding-left: 2rem;
   font-size: 1.7rem;
+`
+
+const ErrorText = styled.span`
+  text-align: left;
+  margin-top: 1rem;
+  margin-left: 1rem;
+  font-size: 1.4rem;
+  color: ${(props) => props.theme.color.mainRed};
 `
 
 const ShowPasswordIcon = styled(BsEye)`
