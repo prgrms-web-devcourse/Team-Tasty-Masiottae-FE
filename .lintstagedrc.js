b86018aad3b35377b7bc27c35475411dcd501ac5
nyoung <75849590+nyoung113@users.noreply.github.com>
2022-08-03 14:53:03 +0900

    feat: 이미지 변환 코드 추가

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,9 +1,9 @@
 module.exports = {
   // Type check TypeScript files
-  '**/*.(ts|tsx)': () => 'npx tsc --noEmit'
+  '**/*.(ts|tsx)': () => 'npx tsc --noEmit',
 
   // Lint then format TypeScript and JavaScript files
-  //  '**/*.(ts|tsx|js)': (filenames) => []
+  '**/*.(ts|tsx|js)': (filenames) => []
 
   // Format MarkDown and JSON
 }
---
+++ src/utils/urlToBlob.ts
@@ -0,0 +1,13 @@
+export function urlToBlob(imgSrc: string) {
+  const byteString = atob(imgSrc.split(',')[1])
+
+  const ab = new ArrayBuffer(byteString.length)
+  const ia = new Uint8Array(ab)
+
+  for (let i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i)
+  }
+  const blob = new Blob([ia], {
+    type: 'image/jpeg'
+  })
+}
