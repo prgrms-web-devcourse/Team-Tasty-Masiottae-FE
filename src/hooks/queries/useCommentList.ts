34eb3ef44536a7916ae6474f28a5756f796c06c8
chogyejin <whrpwls96@naver.com>
2022-08-04 14:49:05 +0900

    fix: id type string에서 number로 변경

--- src/hooks/queries/useCommentList.ts
+++ src/hooks/queries/useCommentList.ts
@@ -2,13 +2,13 @@ import axios from '@lib/axios'
 import { Comment } from '@interfaces'
 import { useQuery } from '@tanstack/react-query'
 
-const getCommentListByMenuId = async (menuId: string) => {
+const getCommentListByMenuId = async (menuId: number) => {
   const { data } = await axios.get<Comment[]>(`/menu/${menuId}/comments`)
 
   return data
 }
 
-export const useCommentList = (menuId: string) => {
+export const useCommentList = (menuId: number) => {
   return useQuery<Comment[], Error>(['comments', menuId], () =>
     getCommentListByMenuId(menuId)
   )
--- src/hooks/queries/useMenu.ts
+++ src/hooks/queries/useMenu.ts
@@ -2,12 +2,12 @@ import axios from '@lib/axios'
 import { Menu } from '@interfaces'
 import { useQuery } from '@tanstack/react-query'
 
-const getMenuById = async (menuId: string) => {
+const getMenuById = async (menuId: number) => {
   const { data } = await axios.get<Menu>(`/menu/${menuId}`)
 
   return data
 }
 
-export const useMenu = (menuId: string) => {
+export const useMenu = (menuId: number) => {
   return useQuery<Menu, Error>(['menu', menuId], () => getMenuById(menuId))
 }
