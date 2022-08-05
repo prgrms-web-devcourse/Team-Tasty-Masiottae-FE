2b013b0a406d992342b774c29b2a0b8ec74b5e3a
bellasimi <gksmf6611@gmail.com>
2022-08-06 02:42:39 +0900

    feat: Input 유효성 검사 결과 메시지 출력 컴포넌트 구현

---
+++ src/components/InputMessage.tsx
@@ -0,0 +1,34 @@
+import styled from '@emotion/styled'
+
+interface Props {
+  isValid?: boolean
+  errorMessage: string
+  successMessage?: string
+}
+
+const InputMessage = ({
+  isValid = false,
+  errorMessage,
+  successMessage
+}: Props) => {
+  const message = isValid ? successMessage : errorMessage
+  return (
+    <Message isValid={isValid} message={message} errorMessage={errorMessage}>
+      {message}
+    </Message>
+  )
+}
+
+type MessageType = Props & { message: string | undefined }
+
+const Message = styled.span<MessageType>`
+  text-align: left;
+  margin-top: 1rem;
+  font-size: 1.4rem;
+  margin-left: 1rem;
+  color: ${(props) =>
+    props.isValid ? props.theme.color.success : props.theme.color.error};
+  display: ${({ message }) => message === '' && 'none'};
+`
+
+export default InputMessage
