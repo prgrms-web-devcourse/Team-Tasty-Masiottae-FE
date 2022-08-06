2ea841ee9cec6db021b3e73fa820cfea3a03f174
ljw0096 <ljw0096@naver.com>
2022-08-07 04:05:45 +0900

    refactor: 메뉴 리스트 테스트용 코드 제거

--- src/components/MenuCardList.tsx
+++ src/components/MenuCardList.tsx
@@ -12,46 +12,25 @@ interface Props {
 const MenuCardList = ({ menuList, divRef }: Props) => {
   return (
     <Container>
-      {[...menuList].reverse().map((menu, idx) => {
-        return (
-          <>
-            <MenuCardWrapper key={idx}>
-              <Link href={`/detail/${menu.id}`}>
-                <a>
-                  <MenuCard
-                    key={idx}
-                    title={menu.title}
-                    franchise={menu.franchise.name}
-                    imageUrl={menu.image}
-                    avatarImageUrl={menu.author.image}
-                    author={menu.author.nickName}
-                    likes={menu.likes}
-                    comments={10}
-                    divRef={menuList.length === idx + 1 ? divRef : null}
-                  />
-                </a>
-              </Link>
-            </MenuCardWrapper>
-            <MenuCardWrapper key={idx}>
-              <Link href={`/detail/${menu.id}`}>
-                <a>
-                  <MenuCard
-                    key={idx}
-                    title={menu.title}
-                    franchise={menu.franchise.name}
-                    imageUrl={menu.image}
-                    avatarImageUrl={menu.author.image}
-                    author={menu.author.nickName}
-                    likes={menu.likes}
-                    comments={10}
-                    divRef={menuList.length === idx + 1 ? divRef : null}
-                  />
-                </a>
-              </Link>
-            </MenuCardWrapper>
-          </>
-        )
-      })}
+      {menuList.map((menu, idx) => (
+        <MenuCardWrapper key={idx}>
+          <Link href={`/detail/${menu.id}`}>
+            <a>
+              <MenuCard
+                key={idx}
+                title={menu.title}
+                franchise={menu.franchise.name}
+                imageUrl={menu.image}
+                avatarImageUrl={menu.author.image}
+                author={menu.author.nickName}
+                likes={menu.likes}
+                comments={10}
+                divRef={menuList.length === idx + 1 ? divRef : null}
+              />
+            </a>
+          </Link>
+        </MenuCardWrapper>
+      ))}
     </Container>
   )
 }
