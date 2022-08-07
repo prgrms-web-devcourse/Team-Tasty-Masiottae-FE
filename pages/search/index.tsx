9d6ef710e6508a3d301a36b277e9047c053f3243
ljw0096 <ljw0096@naver.com>
2022-08-07 17:40:36 +0900

    refactor: FrachiseInfoList 컴포넌트로 분리

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,8 +1,6 @@
 import styled from '@emotion/styled'
-import Link from 'next/link'
 import { useFranchiseList } from '@hooks/queries/useFranchiseList'
-import Avatar from '@components/Avatar'
-import theme from '@constants/theme'
+import FranchiseInfoList from '@components/FranchiseInfoList'
 
 const Category = () => {
   const { franchiseList, isLoading } = useFranchiseList()
@@ -13,33 +11,10 @@ const Category = () => {
           <Header>카테고리</Header>
         </InnerWrapper>
       </FixedWrapper>
-
-      <BoxContainer>
-        <Link href={`/search/전체`}>
-          <a>
-            <FranchiseWrapper>
-              <Avatar size={7} src={'/ALL.png'} isLoading={isLoading} />
-              <FranchiseName>전체</FranchiseName>
-            </FranchiseWrapper>
-          </a>
-        </Link>
-        {franchiseList?.map((franchise) => (
-          <BoxWrapper key={franchise.id}>
-            <Link href={`/search/${franchise.name}`}>
-              <a>
-                <FranchiseWrapper>
-                  <Avatar
-                    size={7}
-                    src={franchise.image}
-                    isLoading={isLoading}
-                  />
-                  <FranchiseName>{franchise.name}</FranchiseName>
-                </FranchiseWrapper>
-              </a>
-            </Link>
-          </BoxWrapper>
-        ))}
-      </BoxContainer>
+      <FranchiseInfoList
+        franchiseList={franchiseList || []}
+        isLoading={isLoading}
+      />
     </>
   )
 }
@@ -72,27 +47,5 @@ const Header = styled.h2`
   font-size: 2.4rem;
   padding: 0.5rem;
 `
-
-const BoxContainer = styled.ul`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  padding-top: 8.5rem;
-`
-
-const FranchiseName = styled.div`
-  font-size: 1.8rem;
-  font-weight: 700;
-`
-
-const FranchiseWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-  padding: 1rem;
-  border-bottom: 1px solid ${theme.color.borderLight};
-`
-
-const BoxWrapper = styled.li``
 
 export default Category
---
+++ src/components/FranchiseInfoList.tsx
@@ -0,0 +1,44 @@
+import styled from '@emotion/styled'
+import Link from 'next/link'
+import { Franchise } from '../interfaces/index'
+import FranchiseInfo from './FranchiseInfo'
+
+interface Props {
+  franchiseList: Franchise[]
+  isLoading: boolean
+}
+
+const FranchiseInfoList = ({ franchiseList, isLoading }: Props) => {
+  return (
+    <BoxContainer>
+      <Link href={`/search/전체`}>
+        <a>
+          <FranchiseInfo
+            franchise={{ id: 0, name: '전체', image: '/ALL.png' }}
+            isLoading={isLoading}
+          />
+        </a>
+      </Link>
+      {franchiseList?.map((franchise) => (
+        <BoxWrapper key={franchise.id}>
+          <Link href={`/search/${franchise.name}`}>
+            <a>
+              <FranchiseInfo franchise={franchise} isLoading={isLoading} />
+            </a>
+          </Link>
+        </BoxWrapper>
+      ))}
+    </BoxContainer>
+  )
+}
+
+const BoxContainer = styled.ul`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-top: 8.5rem;
+`
+
+const BoxWrapper = styled.li``
+
+export default FranchiseInfoList
