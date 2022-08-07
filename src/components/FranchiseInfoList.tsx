9044667bfe800ce0422106d747828968adbbe2e4
ljw0096 <ljw0096@naver.com>
2022-08-07 17:43:41 +0900

    style: 카테고리 Item 구분선 적용

--- src/components/FranchiseInfoList.tsx
+++ src/components/FranchiseInfoList.tsx
@@ -1,3 +1,4 @@
+import theme from '@constants/theme'
 import styled from '@emotion/styled'
 import Link from 'next/link'
 import { Franchise } from '../interfaces/index'
@@ -11,14 +12,17 @@ interface Props {
 const FranchiseInfoList = ({ franchiseList, isLoading }: Props) => {
   return (
     <BoxContainer>
-      <Link href={`/search/전체`}>
-        <a>
-          <FranchiseInfo
-            franchise={{ id: 0, name: '전체', image: '/ALL.png' }}
-            isLoading={isLoading}
-          />
-        </a>
-      </Link>
+      <BoxWrapper>
+        <Link href={`/search/전체`}>
+          <a>
+            <FranchiseInfo
+              franchise={{ id: 0, name: '전체', image: '/ALL.png' }}
+              isLoading={isLoading}
+            />
+          </a>
+        </Link>
+      </BoxWrapper>
+
       {franchiseList?.map((franchise) => (
         <BoxWrapper key={franchise.id}>
           <Link href={`/search/${franchise.name}`}>
@@ -39,6 +43,8 @@ const BoxContainer = styled.ul`
   padding-top: 8.5rem;
 `
 
-const BoxWrapper = styled.li``
+const BoxWrapper = styled.li`
+  border-bottom: 1px solid ${theme.color.borderLight};
+`
 
 export default FranchiseInfoList
