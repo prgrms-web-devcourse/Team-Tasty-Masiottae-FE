404e41bac0fb99e374a2b0f8cbfeb913c9d4e910
Shinyoung Hah <79133602+bellasimi@users.noreply.github.com>
2022-08-09 01:14:05 +0900

    chore: 기본 이미지 등록

--- src/recoil/currentUser.ts
+++ src/recoil/currentUser.ts
@@ -1,11 +1,15 @@
 import { atom } from 'recoil'
 import { User } from '@interfaces'
 
+
+const DEFAULT_USER_IMAGE =
+  'https://user-images.githubusercontent.com/79133602/183457197-175f637c-c28b-4ddc-8cf1-195796fc3231.png'
+
 export const currentUser = atom<User>({
   key: 'currentUser',
   default: {
     id: 1,
-    image: '',
+    image: DEFAULT_USER_IMAGE,
     email: 'example@naver.com',
     nickName: '계란이 좋아',
     snsAccount: 'example@naver.com',
