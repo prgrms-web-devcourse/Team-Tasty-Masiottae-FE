08915641ce7921c3e9e289fb522cddd65e90ad81
bellasimi <gksmf6611@gmail.com>
2022-08-10 17:49:13 +0900

    chore: 조건문 수정

--- src/recoil/currentUser.ts
+++ src/recoil/currentUser.ts
@@ -13,21 +13,22 @@ const initialUser = {
   menuCount: 0
 }
 
-const localStorage = typeof window !== undefined ? window.localStorage : null
-
 const localStorageEffect =
   (key: string) =>
   ({ setSelf, onSet }: any) => {
-    const savedValue = localStorage && localStorage.getItem(key)
+    const savedValue =
+      window !== undefined ? window.localStorage.getItem(key) : null
 
     if (savedValue != null) {
       setSelf(JSON.parse(savedValue) || initialUser)
     }
 
     onSet((newValue: string, _: any, isReset: boolean) => {
-      isReset
-        ? localStorage && localStorage.removeItem(key)
-        : localStorage && localStorage.setItem(key, JSON.stringify(newValue))
+      if (window !== undefined) {
+        isReset
+          ? window.localStorage.removeItem(key)
+          : window.localStorage.setItem(key, JSON.stringify(newValue))
+      }
     })
   }
 
