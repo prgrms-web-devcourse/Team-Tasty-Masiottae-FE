b90422b948a3e43276b6e3098f02ea0f6d7d4fc5
bellasimi <gksmf6611@gmail.com>
2022-08-10 16:44:05 +0900

    fix: 토큰 타입 변경, 및 값 추가

--- src/utils/localToken.ts
+++ src/utils/localToken.ts
@@ -1,9 +1,21 @@
-const LOCAL_TOKEN_KEY = 'tastyToken'
-const LOCAL_TOKEN_EXPIRE_DATE = 'tastyToken_expire'
+import {
+  LOCAL_TOKEN_KEY,
+  LOCAL_TOKEN_EXPIRE_DATE,
+  LOCAL_REFRESH_TOKEN_KEY,
+  LOCAL_REFRESH_TOKEN_EXPIRE_DATE
+} from '@constants/token'
+
+import { User } from '@interfaces'
+
+interface Token {
+  token: string
+  expirationDate: string
+}
 
 interface Data {
-  accessToken: string
-  expirationTime: string
+  accessToken: Token
+  account: User
+  refreshToken: Token
 }
 
 export const getLocalToken = () => {
@@ -12,10 +24,34 @@ export const getLocalToken = () => {
   return token
 }
 
-export const setLocalToken = ({ accessToken, expirationTime }: Data) => {
+export const getTokenData = () => {
+  const tokenData = typeof window !== undefined && {
+    [LOCAL_TOKEN_KEY]: window.localStorage.getItem(LOCAL_TOKEN_KEY),
+    [LOCAL_TOKEN_EXPIRE_DATE]: window.localStorage.getItem(
+      LOCAL_TOKEN_EXPIRE_DATE
+    ),
+    [LOCAL_REFRESH_TOKEN_KEY]: window.localStorage.getItem(
+      LOCAL_REFRESH_TOKEN_KEY
+    ),
+    [LOCAL_REFRESH_TOKEN_EXPIRE_DATE]: window.localStorage.getItem(
+      LOCAL_REFRESH_TOKEN_EXPIRE_DATE
+    )
+  }
+  return tokenData
+}
+
+export const setLocalToken = ({ accessToken, refreshToken }: Data) => {
   if (typeof window !== undefined) {
-    window.localStorage.setItem(LOCAL_TOKEN_KEY, accessToken)
-    window.localStorage.setItem(LOCAL_TOKEN_EXPIRE_DATE, expirationTime)
+    window.localStorage.setItem(LOCAL_TOKEN_KEY, accessToken.token)
+    window.localStorage.setItem(
+      LOCAL_TOKEN_EXPIRE_DATE,
+      accessToken.expirationDate
+    )
+    window.localStorage.setItem(LOCAL_REFRESH_TOKEN_KEY, refreshToken.token)
+    window.localStorage.setItem(
+      LOCAL_REFRESH_TOKEN_EXPIRE_DATE,
+      refreshToken.expirationDate
+    )
   }
 }
 
@@ -23,5 +59,7 @@ export const removeLocalToken = () => {
   if (typeof window !== undefined) {
     window.localStorage.removeItem(LOCAL_TOKEN_KEY)
     window.localStorage.removeItem(LOCAL_TOKEN_EXPIRE_DATE)
+    window.localStorage.removeItem(LOCAL_REFRESH_TOKEN_KEY)
+    window.localStorage.removeItem(LOCAL_REFRESH_TOKEN_EXPIRE_DATE)
   }
 }
