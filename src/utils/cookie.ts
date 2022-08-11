2044ae700af8cfc5a4d0a1384891721f6f7779b4
Shinyoung Hah <79133602+bellasimi@users.noreply.github.com>
2022-08-11 14:56:37 +0900

    feat: 쿠키에 토큰 및 currentUser 정보 담기 (#178)
    * refactor: 토큰 쿠키에서 관리하도록 수정
    
    * chore: react-cookie 설치
    
    * refactor: 기존 localToken 사용하던 부분 쿠키로 수정
    
    * chore: 쿠키 key값 상수화
    
    * feat: token 데이터에 currentUser도 추가

---
+++ src/utils/cookie.ts
@@ -0,0 +1,80 @@
+import {
+  TOKEN_KEY,
+  TOKEN_EXPIRE_DATE,
+  REFRESH_TOKEN_KEY,
+  REFRESH_TOKEN_EXPIRE_DATE,
+  CURRENT_USER
+} from '@constants/token'
+import { User } from '@interfaces'
+import { Cookies } from 'react-cookie'
+
+interface Token {
+  token: string
+  expirationDate: string
+}
+
+interface Data {
+  accessToken: Token
+  account: User
+  refreshToken: Token
+}
+
+const cookies = new Cookies()
+
+export const setCookie = (name: string, value: string, option?: any) => {
+  cookies.set(name, value, { ...option })
+}
+
+export const getCookie = (name: string) => {
+  return cookies.get(name)
+}
+
+export const removeCookie = (name: string) => {
+  cookies.remove(name)
+}
+
+export const setToken = ({ token, expirationDate }: Token) => {
+  setCookie(TOKEN_KEY, token)
+  setCookie(TOKEN_EXPIRE_DATE, expirationDate)
+}
+
+export const getToken = () => {
+  return getCookie(TOKEN_KEY)
+}
+
+export const getTokenData = () => {
+  const tokenData = {
+    [TOKEN_KEY]: getCookie(TOKEN_KEY),
+    [TOKEN_EXPIRE_DATE]: getCookie(TOKEN_EXPIRE_DATE),
+    [REFRESH_TOKEN_KEY]: getCookie(REFRESH_TOKEN_KEY),
+    [REFRESH_TOKEN_EXPIRE_DATE]: getCookie(REFRESH_TOKEN_EXPIRE_DATE),
+    [CURRENT_USER]: getCookie(CURRENT_USER)
+  }
+  return tokenData
+}
+
+export const setTokenData = ({ accessToken, account, refreshToken }: Data) => {
+  setCookie(TOKEN_KEY, accessToken.token, {
+    expires: new Date(accessToken.expirationDate)
+  })
+  setCookie(TOKEN_EXPIRE_DATE, accessToken.expirationDate, {
+    expires: new Date(accessToken.expirationDate)
+  })
+  setCookie(REFRESH_TOKEN_KEY, refreshToken.token, {
+    expires: new Date(refreshToken.expirationDate)
+  })
+  setCookie(REFRESH_TOKEN_EXPIRE_DATE, refreshToken.expirationDate, {
+    expires: new Date(refreshToken.expirationDate)
+  })
+  setCookie(CURRENT_USER, JSON.stringify(account), {
+    expires: new Date(accessToken.expirationDate)
+  })
+}
+
+export const removeTokenData = () => {
+  removeCookie(TOKEN_KEY)
+  removeCookie(TOKEN_EXPIRE_DATE)
+  removeCookie(REFRESH_TOKEN_KEY)
+  removeCookie(REFRESH_TOKEN_EXPIRE_DATE)
+  removeCookie(CURRENT_USER)
+}
