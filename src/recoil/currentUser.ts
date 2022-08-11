d382f54010365bb8e8f46a170b4201357f14f2ea
Shinyoung Hah <79133602+bellasimi@users.noreply.github.com>
2022-08-11 16:53:56 +0900

    chore: currentUser 날라가는 문제만 해결 (#185)

--- src/recoil/currentUser.ts
+++ src/recoil/currentUser.ts
@@ -2,16 +2,34 @@ import { atom } from 'recoil'
 import { User } from '@interfaces'
 import { v1 } from 'uuid'
 import { DEFAULT_USER_IMAGE } from '@constants/image'
+import { setCookie, getCookie } from '@utils/cookie'
+
+export const initialUser = {
+  id: 0,
+  image: DEFAULT_USER_IMAGE,
+  email: '',
+  nickName: '',
+  snsAccount: '',
+  createdAt: '',
+  menuCount: 0
+}
+
+const makeCookieEffect =
+  (key: string) =>
+  ({ setSelf, onSet }: any) => {
+    const savedValue = getCookie(key)
+
+    if (savedValue != null) {
+      setSelf(savedValue || initialUser)
+    }
+
+    onSet((newValue: User, _: any, isReset: boolean) => {
+      isReset ? setCookie(key, '') : setCookie(key, JSON.stringify(newValue))
+    })
+  }
 
 export const currentUser = atom<User>({
   key: `currentUser/${v1()}`,
-  default: {
-    id: 1,
-    image: DEFAULT_USER_IMAGE,
-    email: '',
-    nickName: 'test',
-    snsAccount: '',
-    createdAt: '',
-    menuCount: 0
-  }
+  default: initialUser,
+  effects: [makeCookieEffect('currentUser')]
 })
