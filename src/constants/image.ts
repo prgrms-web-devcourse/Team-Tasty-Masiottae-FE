f39940d30ae84b19616e339124b8913b0dca03c2
bellasimi <gksmf6611@gmail.com>
2022-08-12 20:09:28 +0900

    chore: 로고 이미지 상수화

--- src/constants/image.ts
+++ src/constants/image.ts
@@ -1,6 +1,6 @@
 export const DEFAULT_USER_IMAGE =
   'https://user-images.githubusercontent.com/79133602/183457197-175f637c-c28b-4ddc-8cf1-195796fc3231.png'
 export const SMALL_LOGO =
-  'https://user-images.githubusercontent.com/79133602/184317454-74388943-4e6e-408f-95fd-5a3d0886c2dc.svg'
+  'https://user-images.githubusercontent.com/79133602/184341908-2a4cceeb-4883-43a7-a12f-a4e26935d955.svg'
 export const BIG_LOGO =
-  'https://user-images.githubusercontent.com/79133602/184317687-83f5694c-5710-4b0b-a476-52583b2dfba5.svg'
+  'https://user-images.githubusercontent.com/79133602/184341912-1625fb09-1af2-41a0-b16e-6a2f76db30ef.svg'
