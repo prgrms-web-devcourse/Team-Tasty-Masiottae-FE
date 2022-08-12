0e240afdc86321cb48ce9cea12784521cf8b8fa2
bellasimi <gksmf6611@gmail.com>
2022-08-12 20:21:09 +0900

    chore: og태그 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,7 +18,20 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="맛이 어때" />
+          <meta
+            property="og:image"
+            content="https://user-images.githubusercontent.com/79133602/184341912-1625fb09-1af2-41a0-b16e-6a2f76db30ef.svg"
+          />
+          <meta
+            property="og:description"
+            content="커스텀메뉴 정보를 공유하는 SNS"
+          />
+          <meta property="og:site_name" content="맛이 어때" />
+        </Head>
         <body>
           <Main />
           <div id="portal" />
