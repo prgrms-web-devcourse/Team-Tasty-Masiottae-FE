2f5e2a8447b7564fc991e5045ed9d3d6d3850ed1
bellasimi <gksmf6611@gmail.com>
2022-08-12 18:12:20 +0900

    style: 헤더 최종 디자인 시안 반영

--- src/constants/image.ts
+++ src/constants/image.ts
@@ -1,2 +1,6 @@
 export const DEFAULT_USER_IMAGE =
   'https://user-images.githubusercontent.com/79133602/183457197-175f637c-c28b-4ddc-8cf1-195796fc3231.png'
+export const SMALL_LOGO =
+  'https://user-images.githubusercontent.com/79133602/184317454-74388943-4e6e-408f-95fd-5a3d0886c2dc.svg'
+export const BIG_LOGO =
+  'https://user-images.githubusercontent.com/79133602/184317687-83f5694c-5710-4b0b-a476-52583b2dfba5.svg'
