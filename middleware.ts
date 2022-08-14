027e2a6b8fb8a298e7b24716d848d084286deefc
nyoung <75849590+nyoung113@users.noreply.github.com>
2022-08-15 00:05:10 +0900

    fix / 수정 / 생성 시 버튼 한번만 누르게 처리 (#224)
    * refactor: 생성 / 수정 페이지  리팩토링
    
    * feat: 메뉴 수정/ 생성 버튼  한번만 누르게 변경
    
    * feat: 메뉴 수정 / 추가 버튼 변수로 체크하도록  변경

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,17 @@
 import { NextResponse } from 'next/server'
-import type { NextFetchEvent, NextRequest } from 'next/server'
+import type { NextRequest } from 'next/server'
 import { TOKEN_KEY } from '@constants/token'
 import { SIGNUP_URL, LOGIN_URL, HOME_URL } from '@constants/pageUrl'
 
-export function middleware(req: NextRequest, ev: NextFetchEvent) {
+export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl
   const url = req.nextUrl.clone()
-  if (pathname === SIGNUP_URL && req.cookies.get(TOKEN_KEY)) {
-    url.pathname = HOME_URL
-    return NextResponse.redirect(url)
-  }
   if (pathname !== SIGNUP_URL && !req.cookies.get(TOKEN_KEY)) {
     url.pathname = LOGIN_URL
+    return NextResponse.redirect(url)
+  }
+  if (pathname === SIGNUP_URL && req.cookies.get(TOKEN_KEY)) {
+    url.pathname = HOME_URL
     return NextResponse.redirect(url)
   }
 }
