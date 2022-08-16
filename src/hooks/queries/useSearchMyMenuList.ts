4f65fda15f3fd6472880727aaebc0156d08e36be
ljw0096 <ljw0096@naver.com>
2022-08-16 14:26:40 +0900

    fix: 좋아요 변경시 쿼리 refetch 적용

--- src/hooks/queries/useSearchMyMenuList.ts
+++ src/hooks/queries/useSearchMyMenuList.ts
@@ -32,7 +32,7 @@ export const useSearchMyMenuList = (params: searchParams) => {
   const { keyword, tasteIdList, sort, limit, offset, accountId, option } =
     params
 
-  const { data, isLoading, error, fetchNextPage } = useInfiniteQuery<
+  const { data, isLoading, error, fetchNextPage, refetch } = useInfiniteQuery<
     searchResponse,
     Error
   >(
@@ -64,6 +64,12 @@ export const useSearchMyMenuList = (params: searchParams) => {
     .map((value) => value?.menu)
     .flat()
     .filter((val) => !!val)
+
+  refetch({
+    refetchPage: (page, index) => {
+      return true
+    }
+  })
 
   return {
     menuList,
