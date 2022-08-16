00927403ad655cebdd2e7c62cc69120f59908905
chogyejin <whrpwls96@naver.com>
2022-08-16 16:33:24 +0900

    feat: 페이지 간 라우팅 로딩 처리

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,38 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from '@emotion/react'
 import theme from '@constants/theme'
 import { RecoilRoot } from 'recoil'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   Hydrate,
   QueryClient,
   QueryClientProvider
 } from '@tanstack/react-query'
+import { useRouter } from 'next/router'
+import Spinner from '@components/Spinner'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = React.useState(() => new QueryClient())
+  const [isLoading, setIsLoading] = useState(false)
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleStart = (url: string) => {
+      url !== router.asPath && setIsLoading(true)
+    }
+    const handleComplete = (url: string) => {
+      url === router.asPath && setIsLoading(false)
+    }
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleComplete)
+    router.events.on('routeChangeError', handleComplete)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleComplete)
+      router.events.off('routeChangeError', handleComplete)
+    }
+  }, [router])
 
   return (
     <QueryClientProvider client={queryClient}>
@@ -20,6 +43,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <ThemeProvider theme={theme}>
           <RecoilRoot>
             <Layout>
+              {isLoading && <Spinner />}
               <Component {...pageProps} />
             </Layout>
           </RecoilRoot>
