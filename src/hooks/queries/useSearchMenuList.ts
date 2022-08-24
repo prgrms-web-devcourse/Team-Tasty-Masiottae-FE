5ca84f4a5bf45943af2b971890719b52489e18b4
ljw0096 <ljw0096@naver.com>
2022-08-24 19:10:37 +0900

    fix: searchMenuList 훅에서 refetch 제거

--- src/hooks/queries/useSearchMenuList.ts
+++ src/hooks/queries/useSearchMenuList.ts
@@ -30,7 +30,7 @@ const getMenuList = async (params: searchParams) => {
 export const useSearchMenuList = (params: searchParams) => {
   const { keyword, tasteIdList, sort, limit, franchiseId, offset } = params
 
-  const { data, isLoading, error, fetchNextPage, refetch } = useInfiniteQuery<
+  const { data, isLoading, error, fetchNextPage } = useInfiniteQuery<
     searchResponse,
     Error
   >(
@@ -55,17 +55,10 @@ export const useSearchMenuList = (params: searchParams) => {
     .flat()
     .filter((val) => !!val)
 
-  refetch({
-    refetchPage: (page, index) => {
-      return true
-    }
-  })
-
   return {
     menuList,
     isLoading,
     error,
-    fetchNextPage,
-    refetch
+    fetchNextPage
   }
 }
